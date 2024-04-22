c18af2c1fbb8d931b146b7b58aedf88570037184
Nawaphong Sitaruno <nawaphong.s@arise.tech>
2024-04-22 17:06:01 +0700

    Refactor camera.ts to support both Android and iOS platforms

--- src/services/jsBridge/core/camera.ts
+++ src/services/jsBridge/core/camera.ts
@@ -2,15 +2,20 @@ const triggerNativeGetCameraImage = (
   callback: (base64Image: string) => void,
   callbackError: (errorCode: string, errorDescription: string) => void
 ) => {
-  if (window.JSBridge) {
-    // android
+  if (window.bridge) {
     window.bridge.getCameraImageCallbackError = callbackError;
     window.bridge.getCameraImageCallback = callback;
+  }
+
+  if (window.JSBridge) {
+    // android
+    // window.bridge.getCameraImageCallbackError = callbackError;
+    // window.bridge.getCameraImageCallback = callback;
     window.JSBridge.getCameraImage?.();
   } else if (window.webkit) {
     // ios
-    window.bridge.getCameraImageCallbackError = callbackError;
-    window.bridge.getCameraImageCallback = callback;
+    // window.bridge.getCameraImageCallbackError = callbackError;
+    // window.bridge.getCameraImageCallback = callback;
 
     const message = { name: "getCameraImage" };
     window.webkit.messageHandlers.observer.postMessage(message);
