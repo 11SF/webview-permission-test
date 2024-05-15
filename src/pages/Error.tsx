e6ba412739ee0c6adb862aaae9ce46ce9ec557d0
Nawaphong Sitaruno <nawaphong.s@arise.tech>
2024-05-15 17:15:02 +0700

    add error desc

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -9,9 +9,9 @@ export default function ErrorPage() {
     <div className="h-screen flex flex-col justify-center items-center">
       <div className="text-center flex flex-col gap-[16px]">
         <p className="font-bold text-[18px]">ไม่สามารถทำรายการได้ในขณะนี้</p>
-        <p>
-          กรุณาลองใหม่อีกครั้งในภายหลัง [Error: {location.state?.errorCode}]
-        </p>
+        <p>กรุณาลองใหม่อีกครั้งในภายหลัง</p>
+        <p>Error code: {location.state?.errorCode}</p>
+        <p>Error desc: {location.state?.errorDescription}</p>
         <Button
           className="w-full"
           label="Back"
